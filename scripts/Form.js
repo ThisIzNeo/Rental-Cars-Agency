7e4fdde2e150f370136169ad61ad37f9867a93b1
Younes Menasri <younesbox01@gmail.com>
2024-07-21 10:14:10 +0100

    Revert "v3.5 adding contact form responsive at all screens"
    This reverts commit 6d2afb1c16b11cba05813ce73fa922aa3402443b.

--- scripts/Form.js
+++ scripts/Form.js
@@ -1,15 +1,3 @@
-const contactBtn = document.getElementById("contactUsBtn");
-const closeBtn = document.getElementById("closeForm");
-const formContact = document.getElementById("form-contact")
-
 function FormContact(){
-    contactBtn.addEventListener('click', (e) =>{
-        formContact.classList.add("fromShowen")
-    })
-}
-
-function closeForm(){
-    closeBtn.addEventListener('click', (e) =>{
-        formContact.classList.remove("fromShowen")
-    })
+    
 }
