2173beb66e2f30ac149474eb7ed644da18320ebb
Younes Menasri <younesbox01@gmail.com>
2024-07-21 11:23:02 +0100

    v4.0 adding dataCars from productsData.json

--- scripts/DataCall.js
+++ scripts/DataCall.js
@@ -1,9 +1,31 @@
+const product = document.querySelector('.products');
 
-fetch('productsData.json').then(function (response){
-    return response.json();
-}).then(function (obj) {
-    console.log(obj);
-}).catch(function (error) {
-    console.error('Something went wrong');
-    console.error(error);
-});
+fetch('scripts/productsData.json')
+  .then(res => res.json())
+  .then(prodctsData => {
+    prodctsData.forEach(car => {
+      product.insertAdjacentHTML("beforeend",
+        `
+        <div class="product">
+                    <h3 class="carName">${car.name}</h3>
+                    <p class="carType">${car.type}</p>
+                    <img src="${car.productImg}" alt="Audi a1">
+                    <div class="rent-info">
+                        <div class="capacity">
+                            <div class="icon-inf"><i class='bx bx-user'></i></div>
+                            <div class="text-inf">${car.capacity} Person</div>
+                        </div>
+                        <div class="capacity">
+                            <div class="icon-inf"><i class='bx bx-cog'></i></div>
+                            <div class="text-inf">${car.gear}</div>
+                        </div>
+                    </div>
+                    <div class="rent-option">
+                        <p><b>$${car.price}</b>/day</p>
+                        <button>Rent Car</button>
+                    </div>
+                </div>
+        `
+      );
+    });
+  })
